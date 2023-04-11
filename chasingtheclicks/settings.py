5dcf7bda4dc0703ab18f73bc9cc2496e5c1e2729
Michael <91182988+veeMichael@users.noreply.github.com>
2023-04-10 21:53:49 -0400

    Update settings.py

--- chasingtheclicks/settings.py
+++ chasingtheclicks/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = ''
+# SECRET_KEY = 'django-insecure-$0*5em5lbgr2x%8t2#5xpafvaf4zrwcdh6q5=_%1$g%v_b&x!8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
