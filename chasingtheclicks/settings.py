c40243f0ece07fe27a80e1542464952ab8056504
Michael <91182988+veeMichael@users.noreply.github.com>
2023-04-10 22:03:54 -0400

    Update settings.py

--- chasingtheclicks/settings.py
+++ chasingtheclicks/settings.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SK_DJ
+SECRET_KEY = os.environ.get('SK_DJ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
